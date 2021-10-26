96a07e9d6f991aab5cf04d39f2480df94310685c
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-10-26 13:32:03 -0400

    changes

--- main.py
+++ main.py
@@ -6,20 +6,12 @@ startCamera = st.button("Start Mask Detection", key="1")
 stopCamera = st.button("Stop Mask Detection", key="2")
 
 if startCamera:
-    while cameraOn ==0:
+    while cameraOn==0:
+        st.write("hello")
         st.spinner('Mask Detector Activating')
 
     st.success('Mask Detector Ready!')
     # cv2.destroyAllWindows()
 
-    # detectMask()
-if stopCamera:
-    if cameraOn:
-        cv2.destroyAllWindows()
-
-while cameraOn==0:
-    pass
-while cameraOn:
-    pass
 
 
