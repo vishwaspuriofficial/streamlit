5837ea8c72ec68dbb2f86aa17695f104689a72d4
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-10-26 17:29:01 -0400

    changes

--- main.py
+++ main.py
@@ -92,107 +92,107 @@ except ImportError:
 import av
 
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    {"iceServers": [{"urlss": ["stun:stun.l.google.com:19302"]}]}
 )
 
 
-def model():
-    class OpenCVVideoProcessor(VideoProcessorBase):
-        type: Literal["noop", "cartoon", "edges", "rotate"]
+# def model():
+class OpenCVVideoProcessor(VideoProcessorBase):
+    type: Literal["noop", "cartoon", "edges", "rotate"]
 
-        def __init__(self) -> None:
-            self.type = "noop"
+    def __init__(self) -> None:
+        self.type = "noop"
 
-        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-            img = frame.to_ndarray(format="bgr24")
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        img = frame.to_ndarray(format="bgr24")
 
-            if self.type == "noop":
-                pass
-            elif self.type == "cartoon":
-                # prepare color
-                img_color = cv2.pyrDown(cv2.pyrDown(img))
-                for _ in range(6):
-                    img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-                img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+        if self.type == "noop":
+            pass
+        elif self.type == "cartoon":
+            # prepare color
+            img_color = cv2.pyrDown(cv2.pyrDown(img))
+            for _ in range(6):
+                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
 
-                # prepare edges
-                img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-                img_edges = cv2.adaptiveThreshold(
-                    cv2.medianBlur(img_edges, 7),
-                    255,
-                    cv2.ADAPTIVE_THRESH_MEAN_C,
-                    cv2.THRESH_BINARY,
-                    9,
-                    2,
-                )
-                img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+            # prepare edges
+            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img_edges = cv2.adaptiveThreshold(
+                cv2.medianBlur(img_edges, 7),
+                255,
+                cv2.ADAPTIVE_THRESH_MEAN_C,
+                cv2.THRESH_BINARY,
+                9,
+                2,
+            )
+            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
 
-                # combine color and edges
-                img = cv2.bitwise_and(img_color, img_edges)
-            elif self.type == "edges":
-                # perform edge detection
-                img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-            elif self.type == "rotate":
-                # rotate image
-                rows, cols, _ = img.shape
-                M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-                img = cv2.warpAffine(img, M, (cols, rows))
+            # combine color and edges
+            img = cv2.bitwise_and(img_color, img_edges)
+        elif self.type == "edges":
+            # perform edge detection
+            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+        elif self.type == "rotate":
+            # rotate image
+            rows, cols, _ = img.shape
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+            img = cv2.warpAffine(img, M, (cols, rows))
 
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-    webrtc_ctx = webrtc_streamer(
-        key="opencv-filter",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration=RTC_CONFIGURATION,
-        video_processor_factory=OpenCVVideoProcessor,
-        media_stream_constraints={"video": True, "audio": False},
-        async_processing=True,
+webrtc_ctx = webrtc_streamer(
+    key="opencv-filter",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    video_processor_factory=OpenCVVideoProcessor,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
+
+if webrtc_ctx.video_processor:
+    webrtc_ctx.video_processor.type = st.radio(
+        "Select transform type", ("noop", "cartoon", "edges", "rotate")
     )
 
-    if webrtc_ctx.video_processor:
-        webrtc_ctx.video_processor.type = st.radio(
-            "Select transform type", ("noop", "cartoon", "edges", "rotate")
-        )
+# def maskDetector():
+noseCascade = cv2.CascadeClassifier("haarcascade_mcs_nose.xml")
+mouthCascade = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
 
-def maskDetector():
-    noseCascade = cv2.CascadeClassifier("haarcascade_mcs_nose.xml")
-    mouthCascade = cv2.CascadeClassifier("haarcascade_mcs_mouth.xml")
+class OpenCVVideoProcessor(VideoProcessorBase):
+    type: Literal["noop", "cartoon", "edges", "rotate"]
 
-    class OpenCVVideoProcessor(VideoProcessorBase):
-        type: Literal["noop", "cartoon", "edges", "rotate"]
+    def __init__(self) -> None:
+        self.type = "noop"
 
-        def __init__(self) -> None:
-            self.type = "noop"
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        img = frame.to_ndarray(format="bgr24")
 
-        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-            img = frame.to_ndarray(format="bgr24")
+        if self.type == "noop":
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            if self.type == "noop":
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            nose = noseCascade.detectMultiScale(gray, 1.3, 5)
+            mouth = mouthCascade.detectMultiScale(gray, 1.3, 5)
 
-                nose = noseCascade.detectMultiScale(gray, 1.3, 5)
-                mouth = mouthCascade.detectMultiScale(gray, 1.3, 5)
-
-                if len(nose) != 0 and len(mouth) != 0:
-                    cv2.putText(img, "No Mask!", (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2
+            if len(nose) != 0 and len(mouth) != 0:
+                cv2.putText(img, "No Mask!", (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2
 
 
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-    webrtc_ctx = webrtc_streamer(
-        key="opencv-filter",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration=RTC_CONFIGURATION,
-        video_processor_factory=OpenCVVideoProcessor,
-        media_stream_constraints={"video": True, "audio": False},
-        async_processing=True,
+webrtc_ctx = webrtc_streamer(
+    key="opencv-filter",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    video_processor_factory=OpenCVVideoProcessor,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
+
+if webrtc_ctx.video_processor:
+    webrtc_ctx.video_processor.type = st.radio(
+        "Select transform type", ("noop", "cartoon", "edges", "rotate")
     )
-
-    if webrtc_ctx.video_processor:
-        webrtc_ctx.video_processor.type = st.radio(
-            "Select transform type", ("noop", "cartoon", "edges", "rotate")
-        )
 
 model()
