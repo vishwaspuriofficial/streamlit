c0f4cbb4cb1dac8f1a7eef8701d6cb685ded218b
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-10-27 13:54:46 -0400

    opencv camera

--- main.py
+++ main.py
@@ -1,105 +1,87 @@
+
+
 import streamlit as st
 import cv2
-#
-st.title('Streamlit OpenCV Camera Test')
+import mediapipe as mp
+import random
+import os
 
-MAX_FRAMES = st.slider("Select number of frames to render:", min_value=60, max_value=600, value=300, step=30)
-run = st.button("Click to render server camera")
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
-if run:
-    try:
-        capture = cv2.VideoCapture(0)
-    except:
-        capture = cv2.VideoCapture(1)
-    img_display = st.empty()
-    for i in range(MAX_FRAMES):
-        ret, img = capture.read()
-        img_display.image(img, channels='BGR')
-    capture.release()
-    st.markdown("Render complete")
+from streamlit_webrtc import (
+    AudioProcessorBase,
+    RTCConfiguration,
+    VideoProcessorBase,
+    WebRtcMode,
+    webrtc_streamer,
+)
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # type: ignore
+import av
 
-# import streamlit as st
-# import cv2
-# import mediapipe as mp
-# import random
-# import os
-#
-# mpHands = mp.solutions.hands
-# hands = mpHands.Hands()
-# mpDraw = mp.solutions.drawing_utils
-#
-# from streamlit_webrtc import (
-#     AudioProcessorBase,
-#     RTCConfiguration,
-#     VideoProcessorBase,
-#     WebRtcMode,
-#     webrtc_streamer,
-# )
-# try:
-#     from typing import Literal
-# except ImportError:
-#     from typing_extensions import Literal  # type: ignore
-# import av
-#
-# RTC_CONFIGURATION = RTCConfiguration(
-#     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-# )
-# st.header("Hand Photo Capture")
-# st.write("Press start to turn on Camera and If you want to click a picture, show the camera a peace sign ✌")
-# st.write("If camera dosen't turn on, click the select device button and change the camera input!")
-#
-# def handPhotoCapture():
-#     class OpenCVVideoProcessor(VideoProcessorBase):
-#         type: Literal["hand"]
-#
-#
-#
-#         def __init__(self) -> None:
-#             self.type = "hand"
-#
-#
-#         def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-#             img = frame.to_ndarray(format="bgr24")
-#
-#             def filename():
-#                 i = random.randint(1, 100000000000000)
-#                 check = str(i) + ".jpg"
-#                 if check in os.listdir():
-#                     filename()
-#                 else:
-#                     return i
-#
-#             imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#             results = hands.process(imgRGB)
-#             lmList = []
-#             if results.multi_hand_landmarks:
-#                 for handLms in results.multi_hand_landmarks:
-#                     lmList.append(handLms)
-#                 if len(lmList) != 0:
-#                     if lmList[8][2] < lmList[6][2] and lmList[12][2] < lmList[10][2] and lmList[16][2] > lmList[14][
-#                         2] and lmList[20][2] > lmList[18][2]:
-#                         # Check if index and middle finder are open
-#                         # print("Finger Open")
-#                         i = filename()
-#                         img_name = "{}.jpg".format(i)
-#                         cv2.imwrite(img_name, img)
-#                         print("{} written!".format(img_name))
-#             return av.VideoFrame.from_ndarray(img, format="bgr24")
-#
-#
-#     webrtc_ctx = webrtc_streamer(
-#         key="opencv-filter",
-#         mode=WebRtcMode.SENDRECV,
-#         rtc_configuration=RTC_CONFIGURATION,
-#         video_processor_factory=OpenCVVideoProcessor,
-#         media_stream_constraints={"video": True, "audio": False},
-#         async_processing=True,
-#     )
-#
-#     if webrtc_ctx.video_processor:
-#         webrtc_ctx.video_processor.type = "hand"
-#
-# if __name__ == "__main__":
-#     handPhotoCapture()
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+st.header("Hand Photo Capture")
+st.write("Press start to turn on Camera and If you want to click a picture, show the camera a peace sign ✌")
+st.write("If camera dosen't turn on, click the select device button and change the camera input!")
+
+def handPhotoCapture():
+    class OpenCVVideoProcessor(VideoProcessorBase):
+        type: Literal["hand"]
+
+
+
+        def __init__(self) -> None:
+            self.type = "hand"
+
+
+        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+            img = frame.to_ndarray(format="bgr24")
+
+            def filename():
+                i = random.randint(1, 100000000000000)
+                check = str(i) + ".jpg"
+                if check in os.listdir():
+                    filename()
+                else:
+                    return i
+
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = hands.process(imgRGB)
+            lmList = []
+            if results.multi_hand_landmarks:
+                for handLms in results.multi_hand_landmarks:
+                    lmList.append(handLms)
+                if len(lmList) != 0:
+                    if lmList[8][2] < lmList[6][2] and lmList[12][2] < lmList[10][2] and lmList[16][2] > lmList[14][
+                        2] and lmList[20][2] > lmList[18][2]:
+                        # Check if index and middle finder are open
+                        # print("Finger Open")
+                        i = filename()
+                        img_name = "{}.jpg".format(i)
+                        cv2.imwrite(img_name, img)
+                        print("{} written!".format(img_name))
+            return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+    webrtc_ctx = webrtc_streamer(
+        key="opencv-filter",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_processor_factory=OpenCVVideoProcessor,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
+    )
+
+    if webrtc_ctx.video_processor:
+        webrtc_ctx.video_processor.type = "hand"
+
+if __name__ == "__main__":
+    handPhotoCapture()
 
 
