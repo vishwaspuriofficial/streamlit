6cd7956a16df812454f2dbd6cfd8b3f9ac483382
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-10-27 12:46:34 -0400

    opencv camera

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import streamlit as st
-# import cv2 
+import cv2
 #
 st.title('Streamlit OpenCV Camera Test')
-#
-# MAX_FRAMES = st.slider("Select number of frames to render:", min_value=60, max_value=600, value=300, step=30)
-# run = st.button("Click to render server camera")
-#
-# if run:
-#     capture = cv2.VideoCapture(0)
-#     img_display = st.empty()
-#     for i in range(MAX_FRAMES):
-#         ret, img = capture.read()
-#         img_display.image(img, channels='BGR')
-#     capture.release()
-#     st.markdown("Render complete")
+
+MAX_FRAMES = st.slider("Select number of frames to render:", min_value=60, max_value=600, value=300, step=30)
+run = st.button("Click to render server camera")
+
+if run:
+    try:
+        capture = cv2.VideoCapture(0)
+    except:
+        capture = cv2.VideoCapture(1)
+    img_display = st.empty()
+    for i in range(MAX_FRAMES):
+        ret, img = capture.read()
+        img_display.image(img, channels='BGR')
+    capture.release()
+    st.markdown("Render complete")
 
 # import streamlit as st
 # import cv2
